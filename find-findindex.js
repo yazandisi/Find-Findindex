dbe5732747dc93cfe30f0b6f7e9d72fa364387be
Yazan Eldisi <yazandisi@gmail.com>
2022-03-26 18:51:30 -0500

    Completed

---
+++ find-findindex.js
@@ -0,0 +1,40 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+
+function findUserByUsername(usersArray, username) {
+  usersArray.find(function (val) {
+    return val.username === username;
+  });
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+
+function removeUser(usersArray, username) {
+  let newIndex = usersArray.findIndex(function (val) {
+    return val.username === username;
+  });
+  if (newIndex === -1) return;
+
+  return usersArray.splice(newIndex, 1)[0];
+}
